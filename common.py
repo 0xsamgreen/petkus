748c5214224ef51b60145973cec1f1e35e1b9c75
Sam Green <sam@semiotic.ai>
2021-12-13 22:20:19 -0800

    Restructured Ex. 4.1

--- common.py
+++ common.py
@@ -4,13 +4,13 @@ import sympy as sp
 
 
 def evaluateEncrypted(poly, input_encrypted, prime):
-    '''Homomorphically evaluates a monomial.
+    """Homomorphically evaluates a monomial.
 
     Args:
         poly: A SymPy monomial.
         input_encrypted: Monomial input encrypted by DLP.
         prime: Prime order of the finite field.
-    '''
+    """
 
     # Get the coefficients of the monomial.
     coeffs = sp.Poly(poly).all_coeffs()
@@ -26,11 +26,52 @@ def evaluateEncrypted(poly, input_encrypted, prime):
         # `coeff ` is a SymPy type. Need to convert it to an integer.
         coeff = int(coeff)
         # Exponentiate the encrypted input by the coefficient. E.g.
-        #   (g^s)^c_i = g^(s^c_i) and multiply that by the current value of the 
-        #   accumulator `Eps`. 
+        #   (g^s)^c_i = g^(s^c_i) and multiply that by the current value of the
+        #   accumulator `Eps`.
         Eps *= pow(input_encrypted[idx], coeff, prime)
-    
+
     # Ensure the accumulator is an element of the field.
     Eps = Eps % prime
 
-    return(Eps)
+    return Eps
+
+
+def getCoefficients(y):
+    """Solves interpolation problem using "set of equations with unknowns" method.
+
+    Args:
+        y: Vector of y-axis values that the resulting polynomial must pass through.
+
+    Returns:
+        Coefficients of the interpolated polynomial. Listed in increasing order.
+    """
+
+    # Let f(x) refer to the polynomial learned through interpolation.
+
+    # A d-degree polynomial can fit d+1 points. Our interpolated polynomial
+    #   must fit all points in the y vector.
+    d = len(y) - 1
+
+    # Need to eval f(x) at len(y) arbitrary points. Choosing x = 1, 2, ..., len(y).
+    all_x = np.arange(1, len(y) + 1)
+
+    # `A` matrix will contain powers of x. One row for each x in all_x.
+    A = np.empty(shape=(len(all_x), len(y)))
+    for row, x in enumerate(all_x):
+        for term in range(d + 1):
+            A[row, term] = x ** term
+
+    #     | x_0^0      x_0^1 ...      x0^(d+1)       |
+    # A = | x_1^0      x_1^1 ...      x1^(d+1)       |
+    #     |                  ...                     |
+    #     | x_len(y)^0 x_len(y)^1 ... x_len(y)^(d+1) |
+
+    # Solve A*coeff = y for the `coeff` vector. The `coeff` vector
+    #   will become the coefficients for f(x).
+    A_inv = np.linalg.inv(A)
+    coeff = A_inv @ y
+
+    # Optional: test that `coeff` is the correct solution.
+    assert (A @ coeff == y).all(), "Error with solution."
+
+    return coeff
--- section4.py
+++ section4.py
@@ -1,93 +1,83 @@
-
 from common import *
 
+# Reserving x for use in building SymPy polynomials.
+x = sp.symbols("x")
+
 ##############################################################################
-# Algorithm 3.1
+# Example 4.1
 ##############################################################################
 
-# Setup
-x = sp.symbols('x')
+# 1. Verifier makes it publicly known that they would like a prover to take the
+#   product of the following sequence.
+vec = [2, 1, 3, 2]
 
-# Private to P
-p = x**3 - 3*x**2 + 2*x
+# 2. Prover commits the following polynomial
+t_x = sp.Poly((x - 1) * (x - 2) * (x - 3))
 
-# The commitment which is public
-t = x**2 - 3*x + 2
-
-# 3.1.a
-r = 23 #np.random.randint(1,10)
-print('r', r)
-
-t_r = t.evalf(subs={x:r})
-print('t(r)', t_r)
-
-# V sends r to P
-
-# 3.1.b
-h = sp.simplify(p/t)
-print('h', h)
-
-h_r = h.evalf(subs={x:r})
-print(f'h({r}) {h_r}')
-
-p_r = p.evalf(subs={x:r})
-print(f'p({r}) {p_r}')
-
-# Prover sends h and p to Verifier
-
-# 3.1.c
-assert p_r==t_r*h_r, 'Proof failed'
-print(f'Proof passed! {p_r} == {t_r*h_r}')
-
-
-# Using the "Set of equations with unknowns" interpolation method
-#   to prove a computation of the form x1*x2*x3*...*xn.
-vec = [2,1,3,2]
+# 3. Prover makes polynomials out of the following system of equations:
+#
+#   2 x 1 = 2
+#   2 x 3 = 6
+#   6 x 2 = 12
+#
+# Note that the method by which polynomials are created from that
+#   system is very usual from what you may have seen before.
 
 # left-hand side (the green numbers in the document)
-lhs = [vec[0]]
+l_vec = [vec[0]]
 
 # right-hand side (the blue numbers in the document)
-rhs = vec[1:]
+r_vec = vec[1:]
 
-# intermediate products (the red numbers)
-intermediates = []
+# outputs (the red numbers)
+o_vec = []
 
-# Fill in the LHS, RHS, and output vectors
+# Fill in the LHS and output vectors
 for i in range(len(vec[:-1])):
-    intermediates.append(lhs[i]*rhs[i])
+    print(i, l_vec, r_vec)
+    o_vec.append(l_vec[i] * r_vec[i])
 
     if i < len(vec) - 2:
-        lhs.append(intermediates[i])
+        l_vec.append(o_vec[i])
 
-# Each vector (LHS, RHS, and intermediates) now has n-1 elements.
-# Will now treat those vectors as the outputs for the l(x),
+# Each vector (l_vec, r_vec, and o_vec) now has n-1 elements.
+#  Will now treat those vectors as the outputs for the l(x),
 #   r(x), and o(x) polynomials respectively.
+
 # The next step is to determine the coefficients for those
 #   polynomials using interpolation.
+l_coeff = getCoefficients(l_vec)
+print(f"l(x) coeff.", l_coeff)
+r_coeff = getCoefficients(r_vec)
+print(f"r(x) coeff.", r_coeff)
+o_coeff = getCoefficients(o_vec)
+print(f"o(x) coeff.", o_coeff)
 
-# A degree d polynomial is needed to fit d+1 points.
-# Recall that `lhs`, `rhs`, and `intermediates` all have the same
-#   length.
-d = len(lhs)-1
+# Convert the coefficients to SymPy polynomials.
 
-# Need to eval l(x) at len(lhs) points. Choosing x = 1, 2,  and 3.
-all_x = [1,2,3]
+# Left operand polynomial. `l_x` => l(x)
+l_x = sp.Poly.from_list(reversed(l_coeff), gens=x)
+print("l(x)", l_x)
 
-A = np.empty(shape=(len(all_x), d+1))
+# Right operand polynomial. `r_x` => r(x)
+r_x = sp.Poly.from_list(reversed(r_coeff), gens=x)
+print("r(x)", r_x)
 
-for row, x in enumerate(all_x):
-    for term in range(d+1):
-        A[row, term] = x**term
+# Output polynomial. `o_x` => o(x)
+o_x = sp.Poly.from_list(reversed(o_coeff), gens=x)
+print("o(x)", o_x)
 
-# Solve A*coeff = lhs for the `coeff` vector. The `coeff` vector
-#   will become the coefficients for the lhs(x) polynomial.
-A_inv = np.linalg.inv(A)
-coeff = A_inv@lhs
+# 3. Prover makes the "operation polynomial", p(x):
+p_x = l_x * r_x - o_x
 
-# Optional: test that `coeff` is the correct solution.
-assert (A@coeff == lhs).all(), 'Error with solution.'
+# 4. Prover calculates a cofactor of the operation polynomial, using public
+#   commitment t(x).
+h_x = sp.Poly(sp.simplify(p_x / t_x))
 
-def getCoefficients(y_vals):
-    '''asdf
-    '''
+# Prover sends h(x) to Verifier.
+
+# 5. Verifier checks that t(x)*h(x) = p(x)
+assert h_x * t_x == p_x, "Verification failed."
+
+# The whitepaper doesn't get into this, but the security of the above
+#   example is explained by the Schwartz–Zippel lemma.
